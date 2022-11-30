b21e3a86e8fd983c0979a940b5815bb2b3779a53
bare74 <bjornarenielsen@yahoo.no>
2022-11-30 14:26:07 +0100

    removed unused comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
 import { Route, Routes } from "react-router";
 import Login from "./assets/Login";
-// import Sandbox from "./components/Test";
-// import AppService from "./components/DashBoard";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/test" element={<Sandbox />} />
-        <Route path="/app" element={<AppService />} /> */}
       </Routes>
     </>
   );
